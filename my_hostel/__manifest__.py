89eebb05c0379e3cf55591bdc1a7d7baf65fd1be
ayeshasuchi <ayesha.suchi123@gmail.com>
2025-06-14 22:28:45 +0600

    my_hostel_module

---
+++ my_hostel/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Hostel Management",
+    'summary': "Manage Hostel easily",
+    'description': "Efficiently manage the entire residential facility in the school.",
+    # Supports reStructuredText(RST) format (description is Deprecated),
+    'author': "Ayesha Siddika Suchi (Odoovian)",
+    'website': "https://odoovian.odoo.com/",
+    'category': 'Uncategorized',
+    'version': '17.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        "security/hostel_security.xml",
+        "security/ir.model.access.csv",
+        #"views/hostel.xml",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web/static/src/xml/**/*',
+        ],
+    },
+    'demo': ['demo.xml'],
+}
